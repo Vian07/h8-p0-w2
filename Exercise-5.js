705c52d37f215deb7f4261e110202c2ab109a778
Vian07 <arvianchandra.7@gmail.com>
2018-09-01 00:49:52 +0700

    add tugas 5

---
+++ Exercise-5.js
@@ -0,0 +1,58 @@
+// Soal 1
+var word = 'JavaScript ';
+var second = 'is ';
+var third = 'awesome ';
+var fourth = 'and ';
+var fifth = 'I ';
+var sixth = 'love ';
+var seventh = 'it! ';
+
+console.log(word + second + third + fourth + fifth + sixth + seventh )
+
+// Soal 2
+var word = 'wow JavaScript is so cool';
+var exampleFirstWord = word[0] + word[1] + word[2];
+var secondWord = ('JavaScript');
+var thirdWord = ('is'); 
+var fourthWord = ('so');
+var fifthWord = ('cool'); 
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+//Soal 3
+var word3 = 'wow JavaScript is so cool';
+var exampleFirstWord3 = word.substring(0, 3);
+var secondWord3 = ('JavaScript'); 
+var thirdWord3 = ('is'); 
+var fourthWord3 = ('so'); 
+var fifthWord3 = ('cool'); 
+
+console.log('First Word: ' + exampleFirstWord);
+console.log('Second Word: ' + secondWord);
+console.log('Third Word: ' + thirdWord);
+console.log('Fourth Word: ' + fourthWord);
+console.log('Fifth Word: ' + fifthWord);
+
+//Soal 4
+var word4 = 'wow JavaScript is so cool';
+var exampleFirstWord4 = word.substring(0, 3);
+var secondWord4 = ('JavaScript');
+var thirdWord4 = ('is'); 
+var fourthWord4 = ('so'); 
+var fifthWord4 = ('cool'); 
+
+var firstWordLength = exampleFirstWord4.length;
+var secondWordLength = secondWord4.length;
+var thirdWordLength = thirdWord4.length;
+var fourthWordLength = fourthWord4.length;
+var fifthWordLength = fifthWord4.length;
+
+console.log('First Word: ' + exampleFirstWord + ', with length: ' + firstWordLength);
+console.log('Second Word: ' + secondWord + ', with length: ' + secondWordLength);
+console.log('Third Word: ' + thirdWord + ', with length: ' + thirdWordLength);
+console.log('Fourth Word: ' + fourthWord + ', with length: ' + fourthWordLength);
+console.log('Fifth Word: ' + fifthWord + ', with length: ' + fifthWordLength);
